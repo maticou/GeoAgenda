11d8dcf0c6144d92f9991c49edc74614fa69426a
maticou <maticou123@gmail.com>
2020-05-28 12:08:24 -0400

    Mas enlaces del side menu

--- App/app/src/main/java/com/example/geoagenda/ui/categories/CategoriesViewModel.kt
+++ App/app/src/main/java/com/example/geoagenda/ui/categories/CategoriesViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.geoagenda.ui.slideshow
+package com.example.geoagenda.ui.categories
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class CategoriesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "This is gallery Fragment"
     }
     val text: LiveData<String> = _text
 }
