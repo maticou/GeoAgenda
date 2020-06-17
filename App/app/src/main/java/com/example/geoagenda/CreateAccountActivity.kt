bfaa087d65c0dcd90aac81985a500a5586167709
Gerardo Estrada <gerardoestrada14@hotmail.com>
2020-06-17 19:09:27 -0400

    Registro de Cuenta con Email y Contraseña #2
    - Registro funcionando satisfactoriamente
    - Conexión correcta con Firebase
    - Validación funciona pero no se muestra en pantalla

--- App/app/src/main/java/com/example/geoagenda/CreateAccountActivity.kt
+++ App/app/src/main/java/com/example/geoagenda/CreateAccountActivity.kt
@@ -7,6 +7,10 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
@@ -20,8 +24,11 @@ class CreateAccountActivity : AppCompatActivity() {
     //UI elements
 
     private lateinit var auth: FirebaseAuth
-    lateinit var createAccountButton : Button
+    lateinit var createAccountButton: Button
     lateinit var googleCreateButton: Button
+    //private val GOOGLE_SIGN_IN = 100
+    //val email = newEmailText.text.toString()
+    //val password = newPasswordText.text.toString()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,94 +37,77 @@ class CreateAccountActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
         createAccountButton = findViewById(R.id.createAccountButton)
         googleCreateButton = findViewById(R.id.googleCreateButton)
-        //private val GOOGLE_SIGN_IN = 100
-        val email = newEmailText.text.toString()
-        val password = newPasswordText.text.toString()
 
-        /*createAccountButton.setOnClickListener {
+        //val email = newEmailText.text.toString()
+        //val password = newPasswordText.text.toString()
+
+        createAccountButton.setOnClickListener {
             val email = newEmailText.text.toString()
             val password = newPasswordText.text.toString()
 
-            Log.d( "CreateAccountActivity", "El correo es: " + email)
-            Log.d( "CreateAccountActivity", "La contraseña es: " + password)
-
+            Log.d("CreateAccountActivity", "El correo es: " + email)
+            Log.d("CreateAccountActivity", "La contraseña es: " + password)
 
             auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful && isEmailValid(email)) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d( "CreateAccountActivity", "createUserWithEmail:success")
-                        val user = auth.currentUser
-                        startActivity( Intent(this, MainActivity::class.java))
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w("CreateAccountActivity",  "createUserWithEmail:failure", task.exception)
-                        if(isEmailValid(email) == false)
+                    if (task.isSuccessful && isEmailValid(email) && isPasswordValid(password)) {
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    }
+                    else {
+                        /*if(!isEmailValid(email))
                         {
-                            Toast.makeText(baseContext, "Correo no valido.",
-                                Toast.LENGTH_SHORT).show()
-                            //startActivity( Intent(this, CreateAccountActivity::class.java))
-                        }
-                        /*if(isPasswordValid(password) == false)
-                        {
-                            Toast.makeText(baseContext, "Ingrese una contraseña.",
-                                Toast.LENGTH_SHORT).show()
+                            Toast.makeText(
+                                baseContext, "Email invalido",
+                                Toast.LENGTH_SHORT
+                            ).show()
                         }*/
-                        /*Toast.makeText(baseContext, "Authentication failed.",
-                            Toast.LENGTH_SHORT).show()*/
-                        //startActivity( Intent(this, MainActivity::class.java))
+                        Toast.makeText(
+                            baseContext, "Sign Up failed. Try again after some time.",
+                            Toast.LENGTH_SHORT
+                        ).show()
                     }
                 }
-        }*/
 
-        /*googleCreateButton.setOnClickListener {
-            /*val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build()
+            /* googleCreateButton.setOnClickListener {
+             val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                 .requestIdToken(getString(R.string.default_web_client_id))
+                 .requestEmail()
+                 .build()
 
-            val googleClient = GoogleSignIn.getClient(this, googleConf)
-             */
-
-            startActivity( Intent(this, MainActivity::class.java))
+             val googleClient = GoogleSignIn.getClient(this, googleConf)
 
 
-        }*/
-        setup(email, password)
+             startActivity( Intent(this, MainActivity::class.java))
+
+
+         }*/
+            //setup(email, password)
+        }
+
     }
 
-    private fun setup(email: String, password: String)
-    {
-        title = "Autenticacion"
-        createAccountButton.setOnClickListener {
-            if (email.isNotEmpty()  && password.isNotEmpty())
-            {
-                if(isEmailValid(email))
-                {
-                    auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
-                        if(it.isSuccessful)
-                        {
-                            val user = auth.currentUser
-                            startActivity( Intent(this, MainActivity::class.java))
-                        }
-                        else
-                        {
-                            Toast.makeText(baseContext, "Error.",
-                                Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                }
-                else
-                {
-                    Toast.makeText(baseContext, "Error.",
-                        Toast.LENGTH_SHORT).show()
-                }
-            }
+    private fun isEmailValid(email: String): Boolean {
+        if (!email.isEmpty() or android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            return true
+        } else {
+            return false
         }
     }
 
-    fun isEmailValid(email: String): Boolean {
-        if(!email.isEmpty() or android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches())
+    private fun isPasswordValid(password: String): Boolean {
+        if(!password.isEmpty() or !passwordLength(password))
+        {
+            return true
+        }
+        else{
+            return false
+        }
+
+    }
+
+    private fun passwordLength(password: String): Boolean {
+        if (password.length < 6)
         {
             return true
         }
@@ -126,15 +116,4 @@ class CreateAccountActivity : AppCompatActivity() {
             return false
         }
     }
-
-    /*fun isPasswordValid(password: String): Boolean {
-        if(!password.isEmpty())
-        {
-            return true
-        }
-        else
-        {
-            return false
-        }
-    }*/
 }
