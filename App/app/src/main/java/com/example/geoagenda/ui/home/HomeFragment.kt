963442281a741423d3e26c449e497637c58f6b38
Manuel Gonzalez <mn_gonzalez_16@hotmail.com>
2020-06-18 16:52:12 -0400

    Implementada la funcion para obtener los recordatorios desde la BD
    Se implemento la funcion para obtener los recordatorios desde la BD

--- App/app/src/main/java/com/example/geoagenda/ui/home/HomeFragment.kt
+++ App/app/src/main/java/com/example/geoagenda/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.geoagenda.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,10 +12,16 @@ import androidx.lifecycle.ViewModelProviders
 import com.example.geoagenda.MainActivity
 import com.example.geoagenda.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -29,6 +36,31 @@ class HomeFragment : Fragment() {
         addreminder.setOnClickListener { view ->
             (activity as MainActivity).addReminder()
         }
+
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReferenceFromUrl("https://mementos-da7d9.firebaseio.com/")
+
+        auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+
+        val rootRef = myRef.child(user?.uid.toString()).child("Notas")
+
+        rootRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val values = dataSnapshot.children
+                values.forEach {
+                    //println(it.value)
+                    var newReminder = it.value
+                    println(newReminder)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+            }
+        })
 
         return root
     }
