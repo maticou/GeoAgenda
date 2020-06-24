a3d9a193834f9b470cab7d9263fb2934d9c7630e
Manuel Gonzalez <mn_gonzalez_16@hotmail.com>
2020-06-23 21:33:51 -0400

    Primera version de interfaz para grabar audios
    Se implementó la primera versión del dialog que se usara para grabar los mensajes de voz de un recordatorio.

--- App/app/src/main/java/com/example/geoagenda/AddReminderActivity.kt
+++ App/app/src/main/java/com/example/geoagenda/AddReminderActivity.kt
@@ -1,8 +1,11 @@
 package com.example.geoagenda
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Html
+import android.view.Window
+import android.widget.PopupWindow
 import com.example.geoagenda.ui.reminder.Reminder
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
@@ -45,6 +48,14 @@ class AddReminderActivity : AppCompatActivity() {
         //codigo par el boton de descartar
         discard.setOnClickListener(){
             onBackPressed()
+        }
+
+        add_voice_recording.setOnClickListener{
+            val  voiceRecordingDialog = Dialog(this)
+            voiceRecordingDialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            voiceRecordingDialog.setContentView(R.layout.voice_recoding_popup_layout)
+            voiceRecordingDialog.show()
+
         }
 
 
