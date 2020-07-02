c37fe2b46c5a333be6690380aa1f126acbf17d19
Manuel Gonzalez <mn_gonzalez_16@hotmail.com>
2020-07-01 23:59:44 -0400

    Se implemento la funcionalidad para subir el audio a firebase
    Ahora  se guardan lo audios con el id del recordatorio y se pueden subir al storage de firebase

--- App/app/src/main/java/com/example/geoagenda/AddReminderActivity.kt
+++ App/app/src/main/java/com/example/geoagenda/AddReminderActivity.kt
@@ -5,6 +5,7 @@ import android.app.Dialog
 import android.content.pm.PackageManager
 import android.media.MediaPlayer
 import android.media.MediaRecorder
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock
@@ -17,8 +18,9 @@ import com.example.geoagenda.ui.reminder.Reminder
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_add_reminder.*
-import kotlinx.android.synthetic.main.voice_recoding_popup_layout.*
+import java.io.File
 import java.io.IOException
 
 private const val REQUEST_RECORD_AUDIO_PERMISSION = 200
@@ -31,33 +33,38 @@ class AddReminderActivity : AppCompatActivity() {
     private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
     private var recorder: MediaRecorder? = null
     private var player: MediaPlayer? = null
+    private lateinit var storage: FirebaseStorage
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_reminder)
         ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION)
 
+        //Estos valores modifican datos de la barra de la ventana para crear recordatorios
         val actionBar = supportActionBar
-
         actionBar!!.title = "Agregar Recordatorio"
         actionBar.setTitle(Html.fromHtml("<font color='#FFFFFF'>Agregar Recordatorio </font>"));
         actionBar.setDisplayHomeAsUpEnabled(true)
 
+        auth = FirebaseAuth.getInstance()
+
+        //Valores donde se indica donde se encuentra la base de datos y el almacenamiento de los datos del usuario
         val database = FirebaseDatabase.getInstance()
+        val storageRef = FirebaseStorage.getInstance().getReferenceFromUrl("gs://mementos-da7d9.appspot.com")
         val myRef = database.getReferenceFromUrl("https://mementos-da7d9.firebaseio.com/")
 
-        auth = FirebaseAuth.getInstance()
+        //Parametros que se utilizaran en la creacion de un recordatorio
+        val user = auth.currentUser
+        var itemId = myRef.child(user?.uid.toString()).push()
+        val reminderID = itemId.key.toString()
+        var reminderRecording = ""
 
         //codigo para el boton de guardar
         save.setOnClickListener(){
             val titleInput: String = title_input.text.toString()
             val noteInput: String = note_input.text.toString()
-            val user = auth.currentUser
 
-            var reminder = Reminder("",titleInput, noteInput)
-
-            var itemId = myRef.child(user?.uid.toString()).push()
-            reminder.id = itemId.key.toString()
+            var reminder = Reminder(reminderID,titleInput, noteInput, reminderRecording)
 
             myRef.child(user?.uid.toString()).child("Notas").child(reminder.id).setValue(reminder)
             onBackPressed()
@@ -82,7 +89,7 @@ class AddReminderActivity : AppCompatActivity() {
             var chronometer = voiceRecordingDialog.findViewById(R.id.c_meter) as Chronometer
 
             startRecording.setOnClickListener{
-                start_recording()
+                start_recording(reminderID)
                 chronometer.base = SystemClock.elapsedRealtime()
                 chronometer.start()
                 recording = true
@@ -95,22 +102,35 @@ class AddReminderActivity : AppCompatActivity() {
             }
 
             playRecording.setOnClickListener{
-                play_recording()
+                play_recording(reminderID)
             }
 
             saveRecording.setOnClickListener{
+                //se crea el directorio necesario para guardar el archivo en firebase y luego se almacena
+                var recording = Uri.fromFile(File("${externalCacheDir?.absolutePath}/${reminderID}.3gp"))
+                val recordingsRef = storageRef.child("${user?.uid.toString()}/Audio/${recording.lastPathSegment}")
 
+                reminderRecording = "${externalCacheDir?.absolutePath}/${reminderID}.3gp"
+                var uploadTask = recordingsRef.putFile(recording)
+
+                uploadTask.addOnFailureListener {
+                    println("Ocurrio un error al subir el archivo")
+                }.addOnSuccessListener {
+                    println("El archivo se ha subido correctamente")
+                }
+
+                voiceRecordingDialog.dismiss()
             }
 
             discardRecording.setOnClickListener{
+                //verificar si el archivo esxiste y si existe se elimina, sino solo se cierra el
+                //dialog
 
+                voiceRecordingDialog.dismiss()
             }
 
             voiceRecordingDialog.show()
         }
-
-
-
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -121,11 +141,11 @@ class AddReminderActivity : AppCompatActivity() {
     /*
     Funcion encargada de comenzar la grabacion de audio
      */
-    fun start_recording(){
+    fun start_recording(audio: String){
         recorder = MediaRecorder().apply {
             setAudioSource(MediaRecorder.AudioSource.MIC)
             setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-            setOutputFile("${externalCacheDir?.absolutePath}/audio_test.3gp")
+            setOutputFile("${externalCacheDir?.absolutePath}/${audio}.3gp")
             setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
 
             try {
@@ -150,16 +170,20 @@ class AddReminderActivity : AppCompatActivity() {
 
     }
     
-    private fun play_recording() {
+    private fun play_recording(audio: String) {
         player = MediaPlayer().apply {
             try {
-                setDataSource("${externalCacheDir?.absolutePath}/audio_test.3gp")
+                setDataSource("${externalCacheDir?.absolutePath}/${audio}.3gp")
                 prepare()
                 start()
             } catch (e: IOException) {
                 println(e)
             }
         }
+    }
+
+    private fun delete_recording(audio: String){
+
     }
 
 
