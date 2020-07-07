ca47b6238ef68305b03b509a1fbc4b95c17e320d
Manuel Gonzalez <mn_gonzalez_16@hotmail.com>
2020-07-07 03:40:31 -0400

    Se realizaron cambios en interfaces y se agregaron marcadores al mapa
    Se agrego el campo de nombre a la interfaz para agregar una ubicacion y se agrego la funcionalidad para agregar un marcador al mapa

--- App/app/src/main/java/com/example/geoagenda/ui/addlocation/AddLocationFragment.kt
+++ App/app/src/main/java/com/example/geoagenda/ui/addlocation/AddLocationFragment.kt
@@ -7,10 +7,21 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.geoagenda.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 
-class AddLocationFragment : Fragment() {
+class AddLocationFragment : Fragment(), OnMapReadyCallback {
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
     private lateinit var addLocationViewModel: AddLocationViewModel
+    private var latitude = -35.4333
+    private var longitude = -71.6667
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +31,23 @@ class AddLocationFragment : Fragment() {
         addLocationViewModel =
             ViewModelProviders.of(this).get(AddLocationViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_addlocation, container, false)
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map2) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+
         return root
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        var position = LatLng(latitude, longitude)
+        var marker = googleMap?.addMarker(MarkerOptions().position(position).draggable(true).title("Marcador"))
+        googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 12.0f))
+
+        googleMap?.setOnMapClickListener{ point ->
+            latitude = point.latitude
+            longitude = point.longitude
+
+            marker?.position = LatLng(latitude, longitude)
+        }
     }
 }
