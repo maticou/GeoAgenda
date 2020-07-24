b9498da012421808bf7a16338337f1a2c7035d39
Juanfco <jnfco_18@hotmail.com>
2020-07-23 21:59:43 -0400

    Probando la busqueda de la id mediante correo
    solo se puede buscar ya sabiendo la id de antes, para probarlo hay que hacer click en el botón agregar imagen

--- App/app/src/main/java/com/example/geoagenda/ui/addgroup/User.kt
+++ App/app/src/main/java/com/example/geoagenda/ui/addgroup/User.kt
@@ -1,3 +1,45 @@
 package com.example.geoagenda.ui.addgroup
 
-class User ( val id: String, val email: String)
+class User
+{
+    private var avatar: String = ""
+    private var email: String = ""
+    private var id: String = ""
+    private var provider: String = ""
+    private var username: String = ""
+
+    constructor()
+
+    constructor(avatar:String, email:String,id:String,provider:String,username:String)
+    {
+        this.avatar=avatar
+        this.email=email
+        this.id=id
+        this.provider=provider
+        this.username=username
+    }
+
+    fun getAvatar():String
+    {
+        return avatar
+    }
+    fun getEmail():String
+    {
+        return email
+    }
+    fun getId():String
+    {
+        return id
+    }
+    fun getProvider():String
+    {
+        return provider
+    }
+    fun getUsername():String
+{
+    return username
+}
+
+
+
+}
